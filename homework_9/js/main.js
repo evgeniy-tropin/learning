fad1987b3061e60f8dd924872511c787282887a8
Evgeniy <evgeniy.tropin@flexaspect.com>
2019-01-28 17:24:00 +0200

    hw_9 last changes

--- homework_9/js/main.js
+++ homework_9/js/main.js
@@ -1,95 +1,184 @@
-const creator = document.getElementById("creator");
-const checker = document.getElementById("checkExisting");
-const remover = document.getElementById("remove");
+const creator = document.getElementById('creator');
+const checker = document.getElementById('checkExisting');
+const remover = document.getElementById('remove');
+const price = document.getElementById('getPrice');
+const alcohol = document.getElementById('alcohol');
+const indicator = document.getElementById('indicator');
+const list = document.getElementById('list');
 
 let existingCocktals = [
-    {
-        name : 'Strange Thing',
-        price : 12,
-        isAlcohol : false,
-        type : 'standart'
-    },
-    {
-        name : "Something Interesting",
-        price : 15,
-        isAlcohol : true,
-        type : 'standart'
-    },
-    {
-        name : "Surprise",
-        price : 20,
-        isAlcohol : true,
-        type : 'standart'
-    },
-    {
-        name : "Inspiration",
-        price : 18,
-        isAlcohol : true,
-        type : 'standart'
-    },
-    {
-        name : 'Healthy Body',
-        price : 12,
-        isAlcohol : false,
-        type : 'standart'
-    }
+	{
+		name : 'Strange Thing',
+		price : 12,
+		isAlcohol : false,
+		type : 'standart'
+	},
+	{
+		name : "Something Interesting",
+		price : 15,
+		isAlcohol : true,
+		type : 'standart'
+	},
+	{
+		name : "Surprise",
+		price : 20,
+		isAlcohol : true,
+		type : 'standart'
+	},
+	{
+		name : "Inspiration",
+		price : 18,
+		isAlcohol : true,
+		type : 'standart'
+	},
+	{
+		name : 'Healthy Body',
+		price : 12,
+		isAlcohol : false,
+		type : 'standart'
+	}
 ];
 
 const cocktailsManager = {
-    create : function (name, price, isAlcohol, type) {
-        let cocktailName = {
-            name : name,
-            price : price,
-            isAlcohol : isAlcohol,
-            type : type
-        }
+	create : function (name, price, isAlcohol, type) {
+		let cocktailName = {
+			name : name,
+			price : price,
+			isAlcohol : isAlcohol,
+			type : type
+		}
+		existingCocktals.push(cocktailName);
+	},
+	
+	getPrice : function (name){
+		priceArr = [];
+		let price = existingCocktals.reduce(function(acc, val, idx) {
+			priceArr.push(val.name +": " + val.price);
+			if (existingCocktals.length === priceArr.length){
+				return priceArr;
+			}
+		}, "");
+		priceArr.forEach(function(el) {
+			let cocktal = document.createElement('li');
+			cocktal.innerHTML = el;
+			list.appendChild(cocktal);
+		});
+	},
 
-        existingCocktals.push(cocktailName);
-    },
+	check : function (name) {
+		existingCocktals.forEach(function(el) {
+			let cocktal = document.createElement('li');
+			cocktal.innerHTML = el.name;
+			list.appendChild(cocktal);
+		});
+	},
+	
+	alcohol : function (checkAlcohol) {
+		if (checkAlcohol == true) {
+			indicator.innerHTML = "The following cocktails contain alcohol!";
+			indicator.style.color = '#f00';
+		} else {
+			indicator.innerHTML = "The following cocktails don't contain alcohol!";
+			indicator.style.color = '#0f0';
+		}
+		
+		existingCocktals.forEach(function(el) {
+			if (el.isAlcohol === checkAlcohol) {
+				let cocktal = document.createElement('li');
+				cocktal.innerHTML = el.name;
+				list.appendChild(cocktal);
+			}
+		});
+	},
 
-    check : function (name) {
-        let list = document.getElementById('list');
-
-        existingCocktals.forEach(function(el) {
-            let cocktal = document.createElement('li');
-            cocktal.innerHTML = el.name;
-            list.appendChild(cocktal);
-        });
-    },
-
-    remove : function (name) {
-        console.log(existingCocktals.name);
-        //existingCocktals.splice(cocktal);
-    }
+	remove : function (name) {
+		for (i = 0; i < existingCocktals.length; i++){
+			if (existingCocktals[i].name === name) {
+				existingCocktals.splice(i,1);
+				indicator.innerHTML = "Specified cocktail was successfully removed!";
+				indicator.style.color = '#0f0';
+				break;
+			} else {
+				indicator.innerHTML = "Such cocktail doesn't exist! Please check list of existing cocktails and type one of them";
+				indicator.style.color = '#f00';
+			}
+		}
+		existingCocktals.forEach(function(el) {
+			let cocktal = document.createElement('li');
+			cocktal.innerHTML = el.name;
+			list.appendChild(cocktal);
+		});
+	}
 }
 
 creator.onclick = function() {
-    let name = document.getElementById("name").value;
-    let price = document.getElementById("price").value;
-    let radio = document.getElementsByName("alcohol");
-    let isAlcohol;
-    let type = document.getElementById("type").value;
-
-    for (let i = 0; i < radio.length; i++) {
-        if (radio[i].checked) {
-            isAlcohol = radio[i].value
-        }
-    }
-
-    if (name !== '' || price !== '' || type !== ''){
-        cocktailsManager.create(name, price, isAlcohol, type);
-        document.getElementById('indicator').innerHTML = 'New cocktail was added!';
-        document.getElementById('indicator').style.color = '#0f0';
-    } else {
-        document.getElementById('indicator').innerHTML = 'Fill all fields please!';
-        document.getElementById('indicator').style.color = '#f00';
-    }
+	let name = document.getElementById("name").value;
+	let price = document.getElementById("price").value;
+	let radio = document.getElementsByName("alcohol");
+	let isAlcohol;
+	let type = document.getElementById("type").value;
+	
+	for (let i = 0; i < radio.length; i++) {
+		if (radio[i].checked) {
+			isAlcohol = radio[i].value
+		}
+	}
+	
+	if (isAlcohol == 'true') {
+		isAlcohol = true;
+	} else {
+		isAlcohol = false;
+	}
+	
+	if (name !== '' || price !== '' || type !== ''){
+		cocktailsManager.create(name, price, isAlcohol, type);
+		indicator.innerHTML = "New cocktail was added!";
+		indicator.style.color = '#0f0';
+	} else {
+		indicator.innerHTML = "Specify all cocktail options please!";
+		indicator.style.color = '#f00';
+	}
 };
 
 checker.onclick = function() {
-    cocktailsManager.check(name);
+	indicator.innerHTML = '';
+	list.innerHTML = '';
+	cocktailsManager.check(name);
 }
 
 remover.onclick = function() {
-    cocktailsManager.remove(name);
+	indicator.innerHTML = '';
+	list.innerHTML = '';
+	let name = document.getElementById("name").value;
+	if (name !== ''){
+		cocktailsManager.remove(name);
+	} else {
+		indicator.innerHTML = "Type name of cocktail which you want to remove please!";
+		indicator.style.color = '#f00';
+	}
+}
+
+price.onclick = function() {
+	list.innerHTML = '';
+	let name = document.getElementById("name").value;
+	cocktailsManager.getPrice(name);
+}
+
+alcohol.onclick = function() {
+	list.innerHTML = '';
+	let radio = document.getElementsByName("alcohol");
+	let checkAlcohol;
+	for (let i = 0; i < radio.length; i++) {
+		if (radio[i].checked) {
+			checkAlcohol = radio[i].value
+		}
+	}
+	
+	if (checkAlcohol == 'true') {
+		checkAlcohol = true;
+	} else {
+		checkAlcohol = false;
+	}
+	
+	cocktailsManager.alcohol(checkAlcohol);
 }
